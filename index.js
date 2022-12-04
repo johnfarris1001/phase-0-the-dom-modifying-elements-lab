8da3f593e41a90c6d6ef8071432647a2f2e1fe23
John <johnfarris217@gmail.com>
2022-12-04 15:50:32 -0600

    commit tree f034bff2b25af5c0b7a3a2858b62bb8a65fba09c

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Write your code here!
+
+document.querySelector('main#main').remove();
+
+const newHeader = document.createElement('h1');
+
+newHeader.id = 'victory';
+
+newHeader.textContent = 'YOU-NAME is the champion';
+
+document.body.append(newHeader);
